91934f4ad121d73d299d6ed471b730c1d4c549a9
Amanda D. Barbadora <a.barbadora@gmail.com>
2018-03-11 10:22:52 -0400

    Revert "Updated Rift with materials, animations, and boss prefab changes -MT"

--- TrialsOfTheRiftWC/Assets/Scripts/RiftBossController.cs
+++ TrialsOfTheRiftWC/Assets/Scripts/RiftBossController.cs
@@ -17,7 +17,6 @@ public class RiftBossController : MonoBehaviour {
     private Constants.Global.Color e_color;     // identifies owning team
     private float f_health;    // indicates how much health the boss has
     private RiftController rc_riftController;
-	public Animator animator;
 
     // Getters
     public Constants.Global.Color Color {
@@ -47,8 +46,6 @@ public class RiftBossController : MonoBehaviour {
             Constants.ObjectiveStats.C_DeathBoltCooldown + Constants.ObjectiveStats.C_ForceFieldCooldown);
 
         InvokeRepeating("SpawnRunes", Constants.ObjectiveStats.C_RuneSpawnInterval, Constants.ObjectiveStats.C_RuneSpawnInterval);
-
-		animator = GetComponentInChildren <Animator> ();
     }
 
     /*/////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
@@ -58,7 +55,6 @@ public class RiftBossController : MonoBehaviour {
         foreach (GameObject runes in go_runes) {
             if (!runes.activeSelf) {
                 runes.SetActive(true);
-				animator.SetTrigger ("runeTrigger");
             }
             else {
                 runes.SetActive(false);
@@ -69,7 +65,6 @@ public class RiftBossController : MonoBehaviour {
     private void FireDeathBolts() {
         rc_riftController.FireDeathBolts(e_color);
         go_ForceField.SetActive(false);
-		animator.SetTrigger ("deathboltTrigger");
         Invoke("TurnOnForceField", Constants.ObjectiveStats.C_ForceFieldCooldown);
     }
 
